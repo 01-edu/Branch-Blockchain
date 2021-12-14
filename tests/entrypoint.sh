f3e691d0cfeebf9ffe8ec1f2fada11fc651b8375
Xavier Lavayssière <xavier@bricodeur.com>
2021-12-14 15:37:17 +0100

    Working around docker restrictions

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -2,11 +2,21 @@
 
 set -e
 echo "Exercise: $EXERCISE"
+
 # Depending on testfile intermediate extension, we select the type of test
 if test -f "/app/${EXERCISE}".jtest.js; 
 then
   node /app/test.mjs "/jail/student" "${EXERCISE}"
 else
-  cd /jail
-  npx hardhat test "/jail/tests/${EXERCISE}.test.js"
+  cp "/app/${EXERCISE}.test.js" /jail
+  cp /app/hardhat.config.js /jail
+  cp /app/package.json /jail
+  cp /app/solc /jail
+  ln -s /app/node_modules/ /jail/node_modules
+  
+  cd /jail 
+  # echo "Situation:"
+  # ls
+  # df -h
+  npx hardhat test "/jail/${EXERCISE}.test.js"
 fi
--- tests/hardhat.config.js
+++ tests/hardhat.config.js
@@ -1,16 +1,37 @@
 require("@nomiclabs/hardhat-waffle");
 
+const { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } = require("hardhat/builtin-tasks/task-names");
+const path = require("path");
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args, hre, runSuper) => {
+  if (args.solcVersion === "0.8.10") {
+    const compilerPath = path.join(__dirname, "solc");
+     
+    return {
+      compilerPath,
+      isSolcJs: true, // if you are using a native compiler, set this to false
+      version: args.solcVersion,
+      // this is used as extra information in the build-info files, but other than
+      // that is not important
+      longVersion: "solc-linux-amd64-v0.8.10"
+    }
+  }
+
+  // we just use the default subtask if the version is not 0.8.5
+  return runSuper();
+})
+
 // Ideally sources would be /jail/student. Might be improved XL
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.10",
 
   paths: {
     sources: "./student",
-    tests: "./tests",
+    tests: "./",
     cache: "./cache",
     artifacts: "./artifacts"
   },
