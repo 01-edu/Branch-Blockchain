c56863962afb672bc4852643ef10b512867e7bc4
xalava <xavier@bricodeur.com>
2022-10-20 13:23:42 +0200

    Reorganise test selection

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -15,38 +15,43 @@ else
   set -e
 fi
 
-# testfiles in the main folder are regular js tests
-if test -f "/app/${EXERCISE}".test.js; 
+# Base files are available
+cp /app/package.json /jail
+ln -s /app/node_modules/ /jail/node_modules
+cp -r /root/.cache /jail
+
+# Distinct test environments are triggered depending in the test file folder
+
+if test -f "/app/btc/${EXERCISE}".test.js; 
 then
-  echo ">> Node test <<"
-  # node /app/test.mjs "/jail/student" "${EXERCISE}"
-  bitcoind -conf=/home/xa/.bitcoin/bitcoin.conf&
-  sleep 10
-  if [ $DEBUG = true ]; then
-    cat ~/.bitcoin/bitcoin.conf
-  fi 
+  # Bitcoin related tests (Quest 1)
+  [ $DEBUG ] && ( echo ">> Bitcoin test <<" )
+  cp -r /home/xa/.bitcoin .
+  # bitcoind -conf=/home/xa/.bitcoin/bitcoin.conf -datadir=/home/xa/.bitcoin -daemon -daemonwait 
+  bitcoind -daemon -daemonwait 
 
-  # Only for local testing purposes 
-  cp /app/package.json /jail
-  ln -s /app/node_modules/ /jail/node_modules
-  npx mocha "/app/${EXERCISE}.test.js" 
+  npx mocha "/app/btc/${EXERCISE}.test.js"
 
-elif test -f "/app/test/${EXERCISE}".test.js; then
-  echo ">> Solidity (HH) test <<"
+elif test -f "/app/js/$EXERCISE.test.js"; then
+  # Pure js tests (Quest 1)
+  [ $DEBUG ] && ( echo ">> Pure JS test <<" )
+  npx mocha "/app/js/${EXERCISE}.test.js"
+
+elif test -f "/app/mjs/${EXERCISE}.test.js"; then
+  # Tests relying on the test.mjs (Quest 2)
+  [ $DEBUG ] && ( echo ">> JS test (test.mjs)<<" )
+  node /app/test.mjs ${EXERCISE}
+
+elif test -f "/app/sol/$EXERCISE.test.js"; then
+  # Pure solidity tests (Quest 3)
+  [ $DEBUG ] && ( echo ">> Solidity test (HH) <<" )
   mkdir -p /jail/test
-  cp "/app/test/${EXERCISE}.test.js" /jail/test
+  cp /app/sol/${EXERCISE}.test.js /jail/test/
+  cp /app/hardhat.config.js /jail/
+  cd /jail
 
-  # Might be optionnal
-  cp /app/hardhat.config.js /jail
-  cp /app/package.json /jail
-
-  ln -s /app/node_modules/ /jail/node_modules
-  
-  cd /jail 
-  if [ $DEBUG = true ]; then
-    echo "> DEBUG: Folder & tool <"
-    tree 
-    ls -al
+  if [ $DEBUG ]; then
+    echo "> Debug Info <"
     solc --version
     npx hardhat --version
     df -h
@@ -55,7 +60,10 @@ elif test -f "/app/test/${EXERCISE}".test.js; then
   else 
     npx hardhat test "/jail/test/${EXERCISE}.test.js"
   fi 
+
+# Failure
 else 
-  echo "Entrypoint> No suitable test found"
+  echo "Entrypoint> No suitable test found for $EXERCISE"
+  tree -I node_modules /app 
   exit 1
 fi
