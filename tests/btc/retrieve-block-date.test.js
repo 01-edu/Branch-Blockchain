b9829bd9f520872bc271a5e9835cd2f676ba48cc
jrosendo <jose@01talent.com>
2023-01-15 10:26:42 +0000

    fix(quest1): fix names for quest1 tests

---
+++ tests/btc/retrieve-block-date.test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+const Client = require('bitcoin-core');
+const  {retrieveBlockDate}= require("/jail/student/retrieve-block-date.js")
+
+describe("retrieve block date", function() { 
+  let client, hashLatest, timeLatest
+
+  beforeEach( async function () {
+    client = new Client({ 
+      network: 'regtest', 
+      username: 'leeloo', 
+      password: 'multipass', 
+      port: 18443 //18445
+    })
+    hashLatest = await client.getBestBlockHash()
+    let block = await client.getBlock(hashLatest)
+    timeLatest = block.time
+  })
+
+  it("latest block is ok", async function() {
+    let retrievedTime = await retrieveBlockDate(hashLatest)
+    expect(retrievedTime).to.equal(timeLatest)
+  })
+})
