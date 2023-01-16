484490f9ded8a99702a0a0f675b017455b369de0
jrosendo <jose@01talent.com>
2023-01-16 17:07:09 +0000

    fix: dockerfile and test file
    - removed unnecessary lines from dockerfile
    - updated retrieve-block-date test file

--- tests/btc/retrieve-block-date.test.js
+++ tests/btc/retrieve-block-date.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const Client = require('bitcoin-core');
-const  {retrieveBlockDate}= require("/jail/student/retrieve-block-date.js")
+const  {retrieveBlockDate}= require("/jail/student/retrieveBlockDate.js")
 
 describe("retrieve block date", function() { 
   let client, hashLatest, timeLatest
@@ -10,25 +10,15 @@ describe("retrieve block date", function() {
       network: 'regtest', 
       username: 'leeloo', 
       password: 'multipass', 
-      port: 18443 
+      port: 18443 //18445
     })
     hashLatest = await client.getBestBlockHash()
     let block = await client.getBlock(hashLatest)
     timeLatest = block.time
-    if(DEBUG){
-      dateLatest = new Date(timeLatest*1000)
-      console.log("hashLatest", hashLatest)
-      console.log("timeLatest", timeLatest)     
-      console.log("dateLatest", dateLatest)
-    }
   })
 
   it("latest block is ok", async function() {
     let retrievedTime = await retrieveBlockDate(hashLatest)
-    if(DEBUG){
-      dateRetrievedTime = new Date(retrievedTime*1000)
-      console.log("dateRetrieved", dateRetrievedTime)
-    }
     expect(retrievedTime).to.equal(timeLatest)
   })
 })
