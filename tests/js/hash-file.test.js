d4306f947432f66a44f8b2562079bb4944040fee
nprimo <primo.niccolo@gmail.com>
2023-03-10 18:24:24 +0000

    feat(hash-file): update test
    - adopt mocha framework for testing
    - use import/export keywords instead of require

---
+++ tests/js/hash-file.test.js
@@ -0,0 +1,27 @@
+import crypto from 'crypto'
+import fs from 'fs/promises'
+import assert from 'assert'
+import { hashFile } from '/jail/student/hash-file.js'
+
+const h = e => crypto.createHash('sha256').update(e).digest('hex')
+
+const content = [
+  'Desocupado lector: sin juramento me podrás creer que quisiera que este libro, como hijo del entendimiento, fuera el más hermoso, el más gallardo y más discreto que pudiera imaginarse.',
+  '🌴'.repeat(Math.ceil(Math.random() * 100)),
+  '',
+]
+
+// create test file to pass to student function
+for (const i in content) {
+  fs.writeFile(`./test${i}.txt`, content[i])
+}
+
+describe('HashFile tests', () => {
+  for (const i in content) {
+    it(`Test case ${i}`, async () => {
+      const expected = h(content[i])
+      const submitted = await hashFile(`./test${i}.txt`)
+      assert.equal(submitted, expected)
+    })
+  }
+})
