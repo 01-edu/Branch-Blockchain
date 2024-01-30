5725a13648c211b4fdd7692100d7dd9f4652161b
nprimo <primo.niccolo@gmail.com>
2024-01-30 10:16:10 +0100

    chore(local-node-info): run prettier

--- tests/web3/local-node-info.test.js
+++ tests/web3/local-node-info.test.js
@@ -1,10 +1,14 @@
-const { expect } = require("chai")
+const { expect } = require('chai')
 
 const express = require('express')
 const puppeteer = require('puppeteer-core')
 
-const { sleep, DEBUG, displayBrowserLogs, pp_options } = require('/app/lib/helpers')
-
+const {
+  sleep,
+  DEBUG,
+  displayBrowserLogs,
+  pp_options,
+} = require('/app/lib/helpers')
 
 describe('Local node info', function () {
   let browser
@@ -14,21 +18,18 @@ describe('Local node info', function () {
 
   before(async function () {
     this.timeout(100000)
-    // const app = require('express')();
-    // app.use(require('express-static')('/jail/student/'));
-    // app.use(require('express-static')('/app/lib/'));
     const app = express()
     app.use(express.static('/jail/student/'))
     app.use(express.static('/app/lib/'))
 
-    server = await app.listen(3001)
+    server = app.listen(3001)
 
     browser = await puppeteer.launch(pp_options)
     page = await browser.newPage()
     await page.goto('http://127.0.0.1:3001/local-node-info.html')
     if (DEBUG) displayBrowserLogs(page)
 
-    provider = new ethers.providers.JsonRpcProvider("http://localhost:8545")
+    provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
     await provider.ready
     if (DEBUG) console.log(provider)
   })
@@ -54,8 +55,8 @@ describe('Local node info', function () {
     expect(parseInt(pageBlockNumber)).to.be.equal(blockNumber)
   })
 
-  // TOCHECK : Temporarily disabled because it getBlockNumber seems unreliable. 
-  // it('Should have the correct blocknumber after a transaction', async function() {  
+  // TOCHECK : Temporarily disabled because it getBlockNumber seems unreliable.
+  // it('Should have the correct blocknumber after a transaction', async function() {
   //   const txHash = await signer.sendTransaction({
   //       to: '0x7A7a4EdC679bC4E29F74E32E9eEDd256cd435FBb',
   //       value: ethers.utils.parseEther("0.2"),
@@ -65,6 +66,6 @@ describe('Local node info', function () {
   //   sleep(300)
   //   const pageBlockNumber = await page.$eval('#blockNumber', ci => ci.innerText);
   //   let currentBlockNumber = await provider.getBlockNumber()
-  //   expect(parseInt(pageBlockNumber)).to.be.equal(currentBlockNumber) 
-  // }); 
+  //   expect(parseInt(pageBlockNumber)).to.be.equal(currentBlockNumber)
+  // });
 })
